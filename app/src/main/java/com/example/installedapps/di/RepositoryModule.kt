71c5ae70afc25d6c8793bf64d40b8069fc57446c
01gaM <olga.molchanova.olkamana@gmail.com>
2025-05-13 13:24:09 +0400

    remove apps repository mocks

--- app/src/main/java/com/example/installedapps/di/RepositoryModule.kt
+++ app/src/main/java/com/example/installedapps/di/RepositoryModule.kt
@@ -1,15 +1,27 @@
 package com.example.installedapps.di
 
+import android.content.Context
+import android.content.pm.PackageManager
 import com.example.installedapps.features.app_list.data.repository.AppsRepositoryImpl
 import com.example.installedapps.features.app_list.domain.repository.AppsRepository
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
-    @Binds
-    abstract fun getAppsRepository(appsRepositoryImpl: AppsRepositoryImpl): AppsRepository
+class RepositoryModule {
+    @Provides
+    fun providePackageManager(
+        @ApplicationContext context: Context
+    ): PackageManager {
+        return context.packageManager
+    }
+
+    @Provides
+    fun getAppsRepository(packageManager: PackageManager): AppsRepository {
+        return AppsRepositoryImpl(packageManager)
+    }
 }
--- app/src/main/java/com/example/installedapps/features/app_list/data/repository/AppsRepositoryImpl.kt
+++ app/src/main/java/com/example/installedapps/features/app_list/data/repository/AppsRepositoryImpl.kt
@@ -1,36 +1,60 @@
 package com.example.installedapps.features.app_list.data.repository
 
+import android.content.pm.PackageManager
 import com.example.installedapps.features.app_info.domain.model.AppInfo
 import com.example.installedapps.features.app_list.domain.model.AppListItem
 import com.example.installedapps.features.app_list.domain.repository.AppsRepository
+import java.io.File
+import java.security.MessageDigest
 import javax.inject.Inject
 
-class AppsRepositoryImpl @Inject constructor() : AppsRepository {
+class AppsRepositoryImpl @Inject constructor(
+    private val packageManager: PackageManager
+) : AppsRepository {
     override suspend fun getInstalledApps(): List<AppListItem> {
-        // TODO: remove mock
-        return listOf(
+        val apps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+        return apps.map {
             AppListItem(
-                name = "TestApp1",
-                packageName = "com.example.test1"
-            ),
-            AppListItem(
-                name = "TestApp2",
-                packageName = "com.example.test2"
-            ),
-            AppListItem(
-                name = "TestApp3",
-                packageName = "com.example.test3"
+                name = packageManager.getApplicationLabel(it).toString(),
+                packageName = it.packageName
             )
-        )
+        }.sortedBy { it.name.lowercase() }
     }
 
     override suspend fun getAppInfo(packageName: String): AppInfo? {
-        // TODO: remove mock
-        return AppInfo(
-            name = "MyAppName",
-            version = "1.2.0",
-            packageName = "com.example.test",
-            checkSum = "checksum"
-        )
+        return try {
+            val packageInfo = packageManager.getPackageInfo(packageName, 0)
+            val appInfo = packageInfo.applicationInfo
+            appInfo?.let {
+                val name = packageManager.getApplicationLabel(appInfo).toString()
+                val version = packageInfo.versionName ?: "None"
+                val apkPath = appInfo.sourceDir
+                val checksum = calculateApkChecksum(apkPath)
+
+                AppInfo(
+                    name = name,
+                    version = version,
+                    packageName = packageName,
+                    checkSum = checksum
+                )
+            } ?: return null
+        } catch (e: PackageManager.NameNotFoundException) {
+            null
+        }
+    }
+
+    /**
+     * Вычисление SHA-1 контрольной суммы (checksum) APK-файла
+     */
+    private fun calculateApkChecksum(apkPath: String): String {
+        val digest = MessageDigest.getInstance("SHA-1")
+        File(apkPath).inputStream().use { input ->
+            val buffer = ByteArray(8192)
+            var bytesRead: Int
+            while (input.read(buffer).also { bytesRead = it } > 0) {
+                digest.update(buffer, 0, bytesRead)
+            }
+        }
+        return digest.digest().joinToString("") { "%02x".format(it) }
     }
 }
